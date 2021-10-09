7336a6cb0e0d11f03fe45127ded8ecae01e77ce8
boburwebdev <boburjon.zokirov.2017@gmail.com>
2021-10-10 00:11:07 +0500

    installed firebase, node-sass, moment packages. added basic JSX markup of the Sidebar and useTasks hook with required constants and helper function

--- clone-todoist/src/components/layout/Sidebar.js
+++ clone-todoist/src/components/layout/Sidebar.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { FaChevronDown, FaInbox, FaRegCalendarAlt, FaRegCalendar } from 'react-icons/fa';
 
 const Sidebar = () => {
     return (
-        <aside>
-            This is sidebar
+        <aside className="sidebar" data-testid="sidebar">
+            <ul className="sidebar__generic">
+                <li><span><FaInbox /></span>Inbox</li>
+                <li><span><FaRegCalendar /></span>Today</li>
+                <li><span><FaRegCalendarAlt /></span>Next 7 days</li>
+            </ul>
+
+            <div className="sidebar__middle">
+                <span><FaChevronDown /></span>
+                <h2>Projects</h2>
+            </div>
+
+            <ul className="sidebar__projects">
+                Projects will be here
+            </ul>
+
+            {/* ToDo: Add Project Component Here */}
         </aside>
     )
 }
