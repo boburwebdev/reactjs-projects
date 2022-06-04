59a8a18013679d73e934ef2fd323f05065728f93
boburwebdev <boburjon.zokirov.2017@gmail.com>
2022-06-04 19:24:00 +0500

    added rel="noreferrer" attribute to all links with "target=_blank" attribute to get rid of console warning

--- scrimba_playground/src/BusinessCard/BusinessCardSocialMedia.js
+++ scrimba_playground/src/BusinessCard/BusinessCardSocialMedia.js
@@ -8,22 +8,22 @@ const BusinessCardSocialMedia = () => {
   return (
     <ul className="business_card__social-list">
       <li className="business_card__social-list__item">
-        <a className="business_card__social-list__link" href="https://www.twitter.com" target="_blank">
+        <a className="business_card__social-list__link" href="https://www.twitter.com" target="_blank" rel="noreferrer">
           <img className="business_card__social-list__icon" src={iconTwitter} alt="twitter" />
         </a>
       </li>
       <li className="business_card__social-list__item">
-        <a className="business_card__social-list__link" href="https://www.facebook.com" target="_blank">
+        <a className="business_card__social-list__link" href="https://www.facebook.com" target="_blank" rel="noreferrer">
           <img className="business_card__social-list__icon" src={iconFacebook} alt="facebook" />
         </a>
       </li>
       <li className="business_card__social-list__item">
-        <a className="business_card__social-list__link" href="https://www.instagram.com" target="_blank">
+        <a className="business_card__social-list__link" href="https://www.instagram.com" target="_blank" rel="noreferrer">
           <img className="business_card__social-list__icon" src={iconInstagram} alt="instagram" />
         </a>
       </li>
       <li className="business_card__social-list__item">
-        <a className="business_card__social-list__link" href="https://www.github.com" target="_blank">
+        <a className="business_card__social-list__link" href="https://www.github.com" target="_blank" rel="noreferrer">
           <img className="business_card__social-list__icon" src={iconGithub} alt="github" />
         </a>
       </li>
